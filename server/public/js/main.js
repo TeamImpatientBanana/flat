7d392d40122357c07a6bf967e51515a6d2d47f79
Joe Duncko <dotmaners@gmail.com>
2015-01-17 05:33:20 -0500

    View stuff

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -3,3 +3,8 @@ $(document).ready(function() {
   // Place JavaScript code here...
 
 });
+
+$( "#button" ).click(function() {
+  $("#buttonContainer").css("display","none");
+  $("#form").css("display","block");
+});
